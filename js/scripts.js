b39a0f3a73e50538026c6c7b9dd8c729ea8426ce
Denis Kibet <kibet1816@gmail.com>
2019-05-15 15:12:04 +0300

    Add cascading to styles.css

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#btn").click(function(){
+      $("#tru").slideToggle(550);
+    });
+    $("#gal").click(function(){
+      $(".test").slideToggle(1250);
+    });
+  });
+  
