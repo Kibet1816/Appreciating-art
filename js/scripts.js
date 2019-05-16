0e16d6c85643e8ac6d66d5e3a4dd0e2abaab409b
Denis Kibet <49621287+Kibet1816@users.noreply.github.com>
2019-05-16 16:59:36 +0300

    Removed the loader

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,6 @@ $(document).ready(function(){
   $("#gal").click(function(){
     $(".test").slideToggle(1250);
   });
-    setTimeout(function(){
-      $("body,html").removeClass("no-scroll")
-      $(".loader-ctn").fadeOut();
-    },4000);
-  });
   $("#gal2").click(function(){
     $(".more-graph").slideToggle();
   });
