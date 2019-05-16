d054e72dd6844a15ade4fa6f31d63199b97ec6bf
Denis Kibet <kibet1816@gmail.com>
2019-05-16 12:23:41 +0300

    Added loader to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function(event){
+  // event.preventDefault();
   $("#btn").click(function(){
+    event.preventDefault();
     $("#tru").slideToggle(550);
   });
   $("#gal").click(function(){
     $(".test").slideToggle(1250);
   });
+  $(document).ready(function(){
+    setTimeout(function(){
+      $("body,html").removeClass("no-scroll")
+      $(".loader-ctn").fadeOut();
+    },4000);
+  });
+  
 });
-
