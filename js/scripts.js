6d1bbab767efd5f378b01131ba9497180c4e2a18
Beryl Onyancha <berylonyancha@gmail>
2019-05-16 16:29:13 +0300

    MOdify the page

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $(".col-md-4").click(function() {
+    $(this).children(".text-showing").toggle();
+    $(this).children(".text-hidden").toggle();
+  });
+  $("#submit").click(function(event){
+    var dataName =$("#name1").val();
+    alert("Thank you" + dataName + "for reaching out to us");
+    event.preventDefault();
+  });
+  $(".hover").fadeOut(500);
+  $(".col-md-3").hover(function(){
+    $(this).children(".hover").toggle(500);
+  });
+});
